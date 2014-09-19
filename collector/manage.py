328868435a3c1fc02b16e4077c6395fbe5de0105
Alexander Kislitsky <sand8080@gmail.com>
2014-09-19 18:47:54 +0400

    Test and prod modes handled in manager.py

--- collector/manage.py
+++ collector/manage.py
@@ -2,7 +2,6 @@
 
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
-from flask_script import Command
 from flask_script import Manager
 
 from collector.api import log
@@ -11,21 +10,22 @@ from collector.api.app import db
 from collector.api.db import model
 
 
-class RutTest(Command):
-    """Starts test application
-    """
+def configure_app(mode=None):
+    mode_map = {
+        'test': 'collector.api.config.Testing',
+        'prod': 'collector.api.config.Production'
+    }
+    app.config.from_object(mode_map.get(mode))
+    log.init_logger()
+    return app
 
-    def run(self):
-        app.config.from_object('collector.api.config.Testing')
-        log.init_logger()
-        app.run()
 
-
-manager = Manager(app)
+manager = Manager(configure_app)
+manager.add_option('--mode', help="Acceptable modes. Default: 'test'",
+                   choices=('test', 'prod'), default='test', dest='mode')
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-manager.add_command('runserver_test', RutTest())
 
 
 if __name__ == '__main__':
