aaf91a586a992ccf50ad603680f219149dc927cb
Alexander Kislitsky <akislitsky@mirantis.com>
2014-10-08 20:30:39 +0400

    Installation struct API call
    * jsonschema and model for InstallationStruct added
    * post API call handled
    * DB migration fixed for adding new tables
    * .gitreview added
    * licence text added into files
    
    Blueprint: send-anon-usage
    Change-Id: If83cb6f86fa96d609da4453ed7417cff996c2019

--- collector/collector/test/base.py
+++ collector/collector/test/base.py
@@ -1,3 +1,18 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from alembic.util import CommandError
 from flask import json
 import flask_migrate
 import os
@@ -59,5 +74,9 @@ class DbTest(BaseTest):
         directory = os.path.join(os.path.dirname(__file__),
                                  '..', 'api', 'db', 'migrations')
         with app.app_context():
-            flask_migrate.downgrade(directory=directory)
+            try:
+                flask_migrate.downgrade(directory=directory)
+            except CommandError:
+                # Workaround for the first migration
+                pass
             flask_migrate.upgrade(directory=directory)
