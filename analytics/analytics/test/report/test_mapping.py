6412cc27d229adca39ff3c23da4dd0b73a7cc089
Alexander Kislitsky <akislitsky@mirantis.com>
2014-10-15 13:37:11 +0400

    Oses and nodes distribution report POC
    * doc_type 'installation_struct' renamed to 'structure'
    * mapping for doc_type 'sturcture' in the index 'fuel' added
    * tox handled
    * nodes distribution report fixed
    * mapping tests added
    * license text added into files
    * ranges nodes report implemented
    * custom analyzer for lowercased unsplitted values added 
    
    Blueprint: send-anon-usage
    Change-Id: I26ab5d176d9a6cbcb1f5f12f7c2f50fadc942433

---
+++ analytics/analytics/test/report/test_mapping.py
@@ -0,0 +1,57 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from analytics import config
+from analytics.test.base import ElasticTest
+
+
+class Mapping(ElasticTest):
+
+    def setUp(self):
+        super(Mapping, self).setUp()
+
+    def test_master_node_uid_not_analyzed(self):
+        docs = [
+            {'master_node_uid': 'x0'},
+            {'master_node_uid': 'x0-1'},
+            {'master_node_uid': 'x0_2'},
+            {'master_node_uid': 'x03'},
+            {'master_node_uid': 'x1'},
+            {'master_node_uid': 'x1 1'},
+        ]
+
+        for doc in docs:
+            self.es.index(config.INDEX_FUEL, config.DOC_TYPE_STRUCTURE, doc,
+                          id=doc['master_node_uid'])
+        self.es.indices.refresh(config.INDEX_FUEL)
+
+        # checking master_node_uid tokenizer
+        master_node_uids = {
+            "size": 0,
+            "aggs": {
+                "structs": {
+                    "terms": {
+                        "field": "master_node_uid"
+                    }
+                }
+            }
+        }
+
+        resp = self.es.search(index=config.INDEX_FUEL,
+                              doc_type=config.DOC_TYPE_STRUCTURE,
+                              body=master_node_uids)
+        result = resp['aggregations']['structs']['buckets']
+        # checking that master_node_uids with whitespaces and
+        # non-literal symbols didn't split
+        self.assertEquals(len(docs), len(result))
