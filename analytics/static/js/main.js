8cabc10c59d45a30222262e3d6b9f5d1052de082
Kate Pimenova <kpimenova@mirantis.com>
2014-10-22 18:40:23 +0400

    UI for fuel-stats
    with only fake data currently
    
    Change-Id: I3242d5d94be0c7cf275277e5e65460d68f89783b

---
+++ analytics/static/js/main.js
@@ -0,0 +1,28 @@
+requirejs.config({
+    baseUrl: "js",
+    paths: {
+        jquery: 'jquery-2.1.1.min',
+        elasticsearch: 'elasticsearch.min',
+        d3: 'd3.v3.min',
+        d3pie: 'd3pie.min',
+        d3tip: 'd3.tip.v0.6.3',
+        nv: 'nv.d3.min',
+        app: 'app'
+    },
+    shim: {
+        d3tip: {
+            deps: ['d3'],
+            exports: 'd3tip'
+        },
+        d3pie: {
+            deps: ['d3'],
+            exports: 'd3pie'
+        },
+        nv: {
+            deps: ['d3'],
+            exports: 'nv'
+        }
+    }
+});
+
+require(['app']);
