07d9a346b6e8f9cb7183df543e0146ec450bd353
Alexander Kislitsky <akislitsky@mirantis.com>
2014-11-12 12:43:47 +0300

    Filtering by environment statuses added
    Tests improved
    Test data generation added
    Checking of aggregation requests added
    
    Blueprint: send-anon-usage
    Change-Id: I0b76ff368a8d6e5a2bb8d598195257d052151f45

--- migration/migration/test/report/test_reports.py
+++ migration/migration/test/report/test_reports.py
@@ -12,109 +12,12 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import random
-import uuid
 
 from migration import config
 from migration.test.base import ElasticTest
 
 
 class Reports(ElasticTest):
-
-    def load_data(self):
-        pass
-
-    def gen_id(self, id_range=(0, 1000000)):
-        return random.randint(*id_range)
-
-    def generate_node(
-            self,
-            roles_range=(0, 5),
-            node_roles=('compute', 'controller', 'cinder', 'ceph-osd',
-                        'zabbix', 'mongo'),
-            oses=('Ubuntu', 'CentOs', 'Ubuntu LTS XX'),
-            node_statuses = ('ready', 'discover', 'provisioning',
-                             'provisioned', 'deploying', 'error')
-    ):
-        roles = []
-        for _ in xrange(random.randint(*roles_range)):
-            roles.append(random.choice(node_roles))
-        node = {
-            'id': self.gen_id(),
-            'roles': roles,
-            'os': random.choice(oses),
-            'status': random.choice(node_statuses)
-        }
-        return node
-
-    def generate_cluster(
-            self,
-            nodes_range=(0, 100),
-            oses=('Ubuntu', 'CentOs', 'Ubuntu LTS XX'),
-            release_names=('Juno on CentOS 6.5', 'Juno on Ubuntu 12.04.4'),
-            release_versions=('6.0 TechPreview', '6.0 GA', '6.1'),
-            cluster_statuses=('new', 'deployment', 'stopped', 'operational',
-                              'error', 'remove', 'update', 'update_error'),
-            libvirt_names=('qemu', 'kvm', 'vCenter')
-    ):
-        nodes_num = random.randint(*nodes_range)
-        cluster = {
-            'id': self.gen_id(),
-            'nodes_num': nodes_num,
-            'release': {
-                'os': random.choice(oses),
-                'name': random.choice(release_names),
-                'version': random.choice(release_versions),
-            },
-            'status': random.choice(cluster_statuses),
-            'nodes': [],
-            'attributes': {
-                'libvirt_type': random.choice(libvirt_names)
-            }
-        }
-        for _ in xrange(nodes_num):
-            cluster['nodes'].append(self.generate_node())
-        return cluster
-
-    def generate_structure(
-            self,
-            clusters_num_range=(0, 10),
-            unallocated_nodes_num_range=(0, 20)
-    ):
-        mn_uid = '{}'.format(uuid.uuid4())
-        clusters_num = random.randint(*clusters_num_range)
-        fuel_release = {
-            'release': "XX",
-            'api': 1,
-            'nailgun_sha': "Unknown build",
-            'astute_sha': "Unknown build",
-            'fuellib_sha': "Unknown build",
-            'ostf_sha': "Unknown build",
-            'feature_groups': ['experimental', 'mirantis']
-        }
-
-        structure = {
-            'master_node_uid': mn_uid,
-            'fuel_release': fuel_release,
-            'clusters_num': clusters_num,
-            'clusters': [],
-            'unallocated_nodes_num_range': random.randint(
-                *unallocated_nodes_num_range),
-            'allocated_nodes_num': 0
-        }
-
-        for _ in xrange(clusters_num):
-            cluster = self.generate_cluster()
-            structure['clusters'].append(cluster)
-            structure['allocated_nodes_num'] += cluster['nodes_num']
-        return structure
-
-    def generate_data(self, installations_num=100):
-        for _ in xrange(installations_num):
-            structure = self.generate_structure()
-            self.es.index(config.INDEX_FUEL, config.DOC_TYPE_STRUCTURE,
-                          body=structure, id=structure['master_node_uid'])
-        self.es.indices.refresh(config.INDEX_FUEL)
 
     def test_oses_distribution(self):
         self.generate_data(installations_num=100)
@@ -164,10 +67,35 @@ class Reports(ElasticTest):
                        doc_type=config.DOC_TYPE_STRUCTURE,
                        body=nodes_distribution)
 
-    def test_libvirt_type_distribution(self):
+    def test_envs_distribution(self):
         self.generate_data(installations_num=100)
-        # nodes oses distribution request
-        libvirt_types_req = {
+        envs_distribution = {
+            "size": 0,
+            "aggs": {
+                "envs_distribution": {
+                    "histogram": {
+                        "field": "clusters_num",
+                        "interval": 1
+                    }
+                }
+            }
+        }
+        resp = self.es.search(index=config.INDEX_FUEL,
+                              doc_type=config.DOC_TYPE_STRUCTURE,
+                              body=envs_distribution)
+
+    def test_installations_number(self):
+        installations_num = 150
+        self.generate_data(installations_num=installations_num)
+        resp = self.es.count(index=config.INDEX_FUEL,
+                             doc_type=config.DOC_TYPE_STRUCTURE)
+        self.assertEquals(installations_num, resp['count'])
+
+    def test_filtration(self):
+        installations_num = 100
+        structures = self.generate_data(installations_num=installations_num)
+        statuses = ["operational", "error"]
+        query = {
             "size": 0,
             "aggs": {
                 "clusters": {
@@ -175,14 +103,21 @@ class Reports(ElasticTest):
                         "path": "clusters"
                     },
                     "aggs": {
-                        "attributes": {
-                            "nested": {
-                                "path": "clusters.attributes"
+                        "statuses": {
+                            "filter": {
+                                "terms": {"status": statuses}
                             },
                             "aggs": {
-                                "libvirt_types": {
-                                    "terms": {
-                                        "field": "libvirt_type"
+                                "attributes": {
+                                    "nested": {
+                                        "path": "clusters.attributes"
+                                    },
+                                    "aggs": {
+                                        "libvirt_types": {
+                                            "terms": {
+                                                "field": "libvirt_type"
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -191,6 +126,20 @@ class Reports(ElasticTest):
                 }
             }
         }
-        self.es.search(index=config.INDEX_FUEL,
-                       doc_type=config.DOC_TYPE_STRUCTURE,
-                       body=libvirt_types_req)
+        resp = self.es.search(index=config.INDEX_FUEL,
+                              doc_type=config.DOC_TYPE_STRUCTURE,
+                              body=query)
+
+        # checking filtered clusters num
+        actual_clusters_num = resp['aggregations']['clusters']['statuses']['doc_count']
+        expected_clusters_num = 0
+        for structure in structures:
+            expected_clusters_num += len(filter(lambda c: c['status'] in statuses, structure['clusters']))
+        self.assertEquals(expected_clusters_num, actual_clusters_num)
+
+        # checking number of filtered libvirt types and clusters
+        libvirt_types = resp['aggregations']['clusters']['statuses']['attributes']['libvirt_types']['buckets']
+        self.assertEquals(
+            expected_clusters_num,
+            sum(d['doc_count'] for d in libvirt_types)
+        )
