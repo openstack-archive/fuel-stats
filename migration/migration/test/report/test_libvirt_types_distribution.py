c584b27b9d6557e0513122973cc1e7b723709c12
Alexander Kislitsky <akislitsky@mirantis.com>
2014-11-12 12:43:47 +0300

    Filtering by environment statuses added
    Tests improved
    Test data generation added
    Checking of aggregation requests added
    
    Blueprint: send-anon-usage
    Change-Id: I0b76ff368a8d6e5a2bb8d598195257d052151f45

--- migration/migration/test/report/test_libvirt_types_distribution.py
+++ migration/migration/test/report/test_libvirt_types_distribution.py
@@ -1,4 +1,3 @@
-
 #    Copyright 2014 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,68 +11,14 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from collections import defaultdict
 
 from migration import config
 from migration.test.base import ElasticTest
 
 
-class Reports(ElasticTest):
+class LibvirtTypesDistribution(ElasticTest):
 
-    def test_nodes_distribution(self):
-        structures = self.generate_data(installations_num=100)
-        nodes_distribution = {
-            "size": 0,
-            "aggs": {
-                "nodes_distribution": {
-                    "histogram": {
-                        "field": "allocated_nodes_num",
-                        "interval": 1
-                    }
-                }
-            }
-        }
-        resp = self.es.search(index=config.INDEX_FUEL,
-                              doc_type=config.DOC_TYPE_STRUCTURE,
-                              body=nodes_distribution)
-        distrs = resp['aggregations']['nodes_distribution']['buckets']
-        actual_distr = dict([(d['key'], d['doc_count']) for d in distrs])
-        expected_distr = defaultdict(int)
-        for structure in structures:
-            expected_distr[structure['allocated_nodes_num']] += 1
-        self.assertDictEqual(expected_distr, actual_distr)
-
-    def test_envs_distribution(self):
-        structures = self.generate_data(installations_num=100)
-        envs_distribution = {
-            "size": 0,
-            "aggs": {
-                "envs_distribution": {
-                    "histogram": {
-                        "field": "clusters_num",
-                        "interval": 1
-                    }
-                }
-            }
-        }
-        resp = self.es.search(index=config.INDEX_FUEL,
-                              doc_type=config.DOC_TYPE_STRUCTURE,
-                              body=envs_distribution)
-        distrs = resp['aggregations']['envs_distribution']['buckets']
-        actual_distr = dict([(d['key'], d['doc_count']) for d in distrs])
-        expected_distr = defaultdict(int)
-        for structure in structures:
-            expected_distr[structure['clusters_num']] += 1
-        self.assertDictEqual(expected_distr, actual_distr)
-
-    def test_installations_number(self):
-        installations_num = 150
-        self.generate_data(installations_num=installations_num)
-        resp = self.es.count(index=config.INDEX_FUEL,
-                             doc_type=config.DOC_TYPE_STRUCTURE)
-        self.assertEquals(installations_num, resp['count'])
-
-    def test_filtration(self):
+    def test_report(self):
         installations_num = 100
         structures = self.generate_data(installations_num=installations_num)
         statuses = ["operational", "error"]
@@ -116,11 +61,13 @@ class Reports(ElasticTest):
         filtered_statuses = resp['aggregations']['clusters']['statuses']
         actual_clusters_num = filtered_statuses['doc_count']
         expected_clusters_num = 0
+        total_clusters_num = 0
         for structure in structures:
-            expected_clusters_num += len(
-                filter(lambda c: c['status'] in statuses,
-                       structure['clusters'])
-            )
+            clusters_in_statuses = filter(lambda c: c['status'] in statuses,
+                                          structure['clusters'])
+            expected_clusters_num += len(clusters_in_statuses)
+            total_clusters_num += structure['clusters_num']
+        self.assertGreater(total_clusters_num, actual_clusters_num)
         self.assertEquals(expected_clusters_num, actual_clusters_num)
 
         # checking number of filtered libvirt types and clusters
