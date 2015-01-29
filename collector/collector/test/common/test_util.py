6859ff7007156a8709ccc3912246ea37b539903d
Alexander Kislitsky <akislitsky@mirantis.com>
2015-01-29 13:29:12 +0300

    Collecting OSWL
    Db model created.
    Json scema for API created.
    Blueprints urls moved to app.
    Insert and update for lists of data generalized.
    SQL logging enabled for test mode.
    
    Implements: blueprint openstack-workload-statistics
    Change-Id: If74e4bea0bc9c6a28ce859d775d1b33f36b547a1

--- collector/collector/test/common/test_util.py
+++ collector/collector/test/common/test_util.py
@@ -15,7 +15,15 @@
 from collector.test.base import BaseTest
 
 from collector.api.common.util import build_index
+from collector.api.common.util import get_index
 from collector.api.common.util import split_collection
+
+
+class Indexed(object):
+    """Helper object for testing indexing of objects
+    """
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
 
 
 class TestUtil(BaseTest):
@@ -39,17 +47,29 @@ class TestUtil(BaseTest):
         coll = [
             {'id': 1, 'cd': 2, 'msg': 'm'},
             {'id': 1, 'cd': 2, 'msg': 'm'},
-            {'id': 1, 'cd': 3, 'msg': 'm'},
-            {'id': 2, 'cd': 4, 'msg': 'm'}
+            Indexed(**{'id': 1, 'cd': 3, 'msg': 'm'}),
+            Indexed(**{'id': 2, 'cd': 4, 'msg': 'm'})
         ]
 
         index = build_index(coll, 'id')
-        self.assertEquals(2, len(index))
-        self.assertDictEqual(coll[2], index[(1,)])
-        self.assertDictEqual(coll[3], index[(2,)])
+        self.assertEqual(2, len(index))
+        self.assertEqual(coll[2], index[(1,)])
+        self.assertEqual(coll[3], index[(2,)])
 
         index = build_index(coll, 'id', 'cd')
-        self.assertEquals(3, len(index))
+        self.assertEqual(3, len(index))
         self.assertDictEqual(coll[1], index[(1, 2)])
-        self.assertDictEqual(coll[2], index[(1, 3)])
-        self.assertDictEqual(coll[3], index[(2, 4)])
+        self.assertEqual(coll[2], index[(1, 3)])
+        self.assertEqual(coll[3], index[(2, 4)])
+
+    def test_get_index(self):
+        checks = [
+            (Indexed(**{'one': 1, 'two': 2}), ('one', ), (1,)),
+            (Indexed(**{'one': 1, 'two': 2}), ('one', 'two'), (1, 2)),
+            (Indexed(**{'one': 1, 'two': 2}), (), ()),
+        ]
+        for obj, fields, idx in checks:
+            self.assertTupleEqual(
+                idx,
+                get_index(obj, *fields)
+            )
