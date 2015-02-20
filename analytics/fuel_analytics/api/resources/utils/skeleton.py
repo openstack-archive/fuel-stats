e129befac813c1ca7534c8a691a3b0477deff058
Alexander Kislitsky <akislitsky@mirantis.com>
2015-02-20 12:16:29 +0300

    Get rid of ElasticSearch in export to CSV
    ElasticSearch removed from requirements.txt.
    Generation of installation structures added.
    Network configuration added to skeleton.
    Tox configuration fixed.
    
    Change-Id: I533519d14ce0f49c70a6e2c454ae488e593d76ae
    Implements: blueprint openstack-workload-statistics

--- analytics/fuel_analytics/api/resources/utils/skeleton.py
+++ analytics/fuel_analytics/api/resources/utils/skeleton.py
@@ -14,98 +14,104 @@
 from fuel_analytics.api.common import consts
 
 INSTALLATION_INFO_SKELETON = {
-    'allocated_nodes_num': None,
-    'clusters': [
-        {
-            'attributes': {
-                'assign_public_to_all_nodes': None,
-                'ceilometer': None,
-                'debug_mode': None,
-                'ephemeral_ceph': None,
-                'heat': None,
-                'images_ceph': None,
-                'images_vcenter': None,
-                'iser': None,
-                'kernel_params': None,
-                'libvirt_type': None,
-                'mellanox': None,
-                'mellanox_vf_num': None,
-                'murano': None,
-                'nsx': None,
-                'nsx_replication': None,
-                'nsx_transport': None,
-                'objects_ceph': None,
-                'osd_pool_size': None,
-                'provision_method': None,
-                'sahara': None,
-                'syslog_transport': None,
-                'use_cow_images': None,
-                'vcenter': None,
-                'vlan_splinters': None,
-                'vlan_splinters_ovs': None,
-                'volumes_ceph': None,
-                'volumes_lvm': None,
-                'volumes_vmdk': None
-            },
-            'fuel_version': None,
-            'id': None,
-            'is_customized': None,
-            'mode': None,
-            'net_provider': None,
-            'node_groups': [{'id': None, 'nodes': [{}]}],
-            'nodes': [
-                {
-                    'bond_interfaces': [
-                        {'id': None, 'slaves': [{}]}
-                    ],
-                    'error_type': None,
-                    'group_id': None,
-                    'id': None,
-                    'manufacturer': None,
-                    'nic_interfaces': [{'id': None}],
-                    'online': None,
-                    'os': None,
-                    'pending_addition': None,
-                    'pending_deletion': None,
-                    'pending_roles': [{}],
-                    'platform_name': None,
-                    'roles': [{}],
-                    'status': None
-                }
-            ],
-            'nodes_num': None,
-            'openstack_info': {
-                'images': [{'size': None, 'unit': None}],
-                'nova_servers_count': None
-            },
-            'release': {'name': None, 'os': None, 'version': None},
-            'status': None
+    'structure': {
+        'allocated_nodes_num': None,
+        'clusters': [
+            {
+                'attributes': {
+                    'assign_public_to_all_nodes': None,
+                    'ceilometer': None,
+                    'debug_mode': None,
+                    'ephemeral_ceph': None,
+                    'heat': None,
+                    'images_ceph': None,
+                    'images_vcenter': None,
+                    'iser': None,
+                    'kernel_params': None,
+                    'libvirt_type': None,
+                    'mellanox': None,
+                    'mellanox_vf_num': None,
+                    'murano': None,
+                    'nsx': None,
+                    'nsx_replication': None,
+                    'nsx_transport': None,
+                    'objects_ceph': None,
+                    'osd_pool_size': None,
+                    'provision_method': None,
+                    'sahara': None,
+                    'syslog_transport': None,
+                    'use_cow_images': None,
+                    'vcenter': None,
+                    'vlan_splinters': None,
+                    'vlan_splinters_ovs': None,
+                    'volumes_ceph': None,
+                    'volumes_lvm': None,
+                    'volumes_vmdk': None
+                },
+                'fuel_version': None,
+                'id': None,
+                'is_customized': None,
+                'mode': None,
+                'net_provider': None,
+                'node_groups': [{'id': None, 'nodes': [{}]}],
+                'nodes': [
+                    {
+                        'bond_interfaces': [
+                            {'id': None, 'slaves': [{}]}
+                        ],
+                        'error_type': None,
+                        'group_id': None,
+                        'id': None,
+                        'manufacturer': None,
+                        'nic_interfaces': [{'id': None}],
+                        'online': None,
+                        'os': None,
+                        'pending_addition': None,
+                        'pending_deletion': None,
+                        'pending_roles': [{}],
+                        'platform_name': None,
+                        'roles': [{}],
+                        'status': None
+                    }
+                ],
+                'nodes_num': None,
+                'network_configuration': {
+                    'segmentation_type': None,
+                    'net_l23_provider': None,
+                    'net_manager': None,
+                    'fixed_networks_vlan_start': None,
+                    'fixed_network_size': None,
+                    'fixed_networks_amount': None
+                },
+                'release': {'name': None, 'os': None, 'version': None},
+                'status': None
+            }
+        ],
+        'clusters_num': None,
+        'fuel_release': {
+            'api': None,
+            'astute_sha': None,
+            'build_id': None,
+            'build_number': None,
+            'feature_groups': [{}],
+            'fuellib_sha': None,
+            'fuelmain_sha': None,
+            'nailgun_sha': None,
+            'ostf_sha': None,
+            'production': None,
+            'release': None
+        },
+        'unallocated_nodes_num': None,
+        'user_information': {
+            'company': None,
+            'contact_info_provided': None,
+            'email': None,
+            'name': None
         }
-    ],
-    'clusters_num': None,
-    'creation_date': None,
-    'fuel_release': {
-        'api': None,
-        'astute_sha': None,
-        'build_id': None,
-        'build_number': None,
-        'feature_groups': [{}],
-        'fuellib_sha': None,
-        'fuelmain_sha': None,
-        'nailgun_sha': None,
-        'ostf_sha': None,
-        'production': None,
-        'release': None
     },
     'master_node_uid': None,
     'modification_date': None,
-    'unallocated_nodes_num': None,
-    'user_information': {
-        'company': None,
-        'contact_info_provided': None,
-        'email': None,
-        'name': None
-    }
+    'creation_date': None
 }
 
 OSWL_SKELETONS = {
