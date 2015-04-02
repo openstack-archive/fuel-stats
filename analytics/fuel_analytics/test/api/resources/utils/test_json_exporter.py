74a4fec2973c01ecd7c9fb686a93d526c34b8483
Alexander Kislitsky <akislitsky@mirantis.com>
2015-04-02 17:01:34 +0300

    Json exporters for stats added
    For system tests purposes http handlers for fetching stats
    added into analytics.
    Implemented OSWLs exporters:
    - /api/v1/json/oswls/<master_node_uid>,
    - /api/v1/json/oswls/<master_node_uid>/<resource_type>
    Implemented installation info exporter:
    - /api/v1/json/installation_info/<master_node_uid>
    Implemented action logs exporter:
    - /api/v1/json/action_logs/<master_node_uid>
    
    Change-Id: I746b43c387c90b20c70d5160d92859b4b44998c1
    Closes-Bug: #1438228

---
+++ analytics/fuel_analytics/test/api/resources/utils/test_json_exporter.py
@@ -0,0 +1,137 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from flask import request
+import json
+import mock
+import six
+
+from fuel_analytics.test.api.resources.utils.inst_structure_test import \
+    InstStructureTest
+from fuel_analytics.test.api.resources.utils.oswl_test import \
+    OswlTest
+from fuel_analytics.test.base import DbTest
+
+from fuel_analytics.api.app import app
+from fuel_analytics.api.resources import json_exporter
+
+
+class JsonExporterTest(InstStructureTest, OswlTest, DbTest):
+
+    def test_jsonify_collection(self):
+        variants = [[], [{}], [{'a': 'b'}, {'c': 'd'}]]
+        for variant in variants:
+            it = iter(variant)
+            jsonified = six.text_type(''.join(
+                json_exporter._jsonify_collection(it)))
+            restored = json.loads(jsonified)
+            self.assertItemsEqual(variant, restored)
+
+    def test_get_installation_info_not_found(self):
+        with app.test_request_context():
+            resp = self.client.get('/api/v1/json/installation_info/xxxx')
+            self.check_response_error(resp, 404)
+
+    def test_get_installation_info(self):
+        structs = self.get_saved_inst_structures(installations_num=10)
+        with app.test_request_context():
+            for struct in structs:
+                url = '/api/v1/json/installation_info/{}'.format(
+                    struct.master_node_uid)
+                resp = self.client.get(url)
+                self.check_response_ok(resp)
+                # Checking response is json
+                json.loads(resp.data)
+
+    def test_get_oswls(self):
+        num = 10
+        for resource_type in self.RESOURCE_TYPES:
+            oswls = self.get_saved_oswls(num, resource_type)
+            structs = self.get_saved_inst_structs(oswls)
+            with app.test_request_context():
+                for struct in structs:
+                    url = '/api/v1/json/oswls/{}'.format(
+                        struct.master_node_uid)
+                    resp = self.client.get(url)
+                    self.check_response_ok(resp)
+                    # Checking response is json
+                    json.loads(resp.data)
+
+    def test_get_oswls_by_resource_type(self):
+        num = 10
+        for resource_type in self.RESOURCE_TYPES:
+            oswls = self.get_saved_oswls(num, resource_type)
+            structs = self.get_saved_inst_structs(oswls)
+            with app.test_request_context():
+                for struct in structs:
+                    url = '/api/v1/json/oswls/{}/{}'.format(
+                        struct.master_node_uid, resource_type)
+                    resp = self.client.get(url)
+                    self.check_response_ok(resp)
+                    # Checking response is json
+                    json.loads(resp.data)
+
+    def test_get_action_logs(self):
+        structs = self.get_saved_inst_structures(installations_num=10)
+        self.get_saved_action_logs(structs)
+        with app.test_request_context():
+            for struct in structs:
+                url = '/api/v1/json/action_logs/{}'.format(
+                    struct.master_node_uid)
+                resp = self.client.get(url)
+                self.check_response_ok(resp)
+                # Checking response is json
+                json.loads(resp.data)
+
+    def test_get_dict_param(self):
+        # Pairs of param_name, param_value, expected
+        name = 'param_name'
+        variants = (
+            ('wrong_name', {}, {}),
+            (name, {}, {}), (name, None, {}), (name, 'a', {}),
+            (name, 1, {}), (name, [], {}), (name, (), {}),
+            (name, {'a': 'b'}, {'a': 'b'})
+        )
+        with app.test_request_context():
+            for param_name, param_value, expected in variants:
+                with mock.patch.object(request, 'args',
+                                       {param_name: param_value}):
+                    self.assertDictEqual(
+                        json_exporter.get_dict_param(name),
+                        expected
+                    )
+
+    def test_get_paging_params(self):
+        name = 'paging_params'
+        limit_default = app.config.get('JSON_DB_DEFAULT_LIMIT')
+        variants = (
+            (name, {}, {'limit': limit_default, 'offset': 0}),
+            (name, [], {'limit': limit_default, 'offset': 0}),
+            (name, 4, {'limit': limit_default, 'offset': 0}),
+            ('wrong_name', 4, {'limit': limit_default, 'offset': 0}),
+            (name, {'trash': 'x'}, {'limit': limit_default, 'offset': 0}),
+            (name, {'limit': limit_default + 1}, {'limit': limit_default + 1,
+                                                  'offset': 0}),
+            (name, {'limit': limit_default + 1, 'offset': 50},
+             {'limit': limit_default + 1, 'offset': 50}),
+        )
+
+        with app.test_request_context():
+            for param_name, param_value, expected in variants:
+                with mock.patch.object(request, 'args',
+                                       {param_name: param_value}):
+                    self.assertDictEqual(
+                        json_exporter.get_paging_params(),
+                        expected
+                    )
