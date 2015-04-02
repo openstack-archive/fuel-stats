41749ed3d8f7266f0c13c83a6b40770645aafe17
Alexander Kislitsky <akislitsky@mirantis.com>
2015-04-02 17:01:34 +0300

    Json exporters for stats added
    For system tests purposes http handlers for fetching stats
    added into analytics.
    Implemented OSWLs exporters:
    - /api/v1/json/oswls/<master_node_uid>,
    - /api/v1/json/oswls/<master_node_uid>/<resource_type>
    Implemented installation info exporter:
    - /api/v1/json/installation_info/<master_node_uid>
    Implemented action logs exporter:
    - /api/v1/json/action_logs/<master_node_uid>
    
    Change-Id: I746b43c387c90b20c70d5160d92859b4b44998c1
    Closes-Bug: #1438228

--- analytics/fuel_analytics/api/app.py
+++ analytics/fuel_analytics/api/app.py
@@ -18,16 +18,24 @@ import flask_sqlalchemy
 import six
 
 from fuel_analytics.api.errors import DateExtractionError
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 # Registering blueprints
 from fuel_analytics.api.resources.csv_exporter import bp as csv_exporter_bp
+from fuel_analytics.api.resources.json_exporter import bp as json_exporter_bp
 
 app.register_blueprint(csv_exporter_bp, url_prefix='/api/v1/csv')
+app.register_blueprint(json_exporter_bp, url_prefix='/api/v1/json')
 
 
 @app.errorhandler(DateExtractionError)
 def date_parsing_error(error):
     return make_response(six.text_type(error), 400)
+
+
+@app.errorhandler(NoResultFound)
+def db_object_not_found(error):
+    return make_response(six.text_type(error), 404)
---
+++ analytics/fuel_analytics/api/resources/json_exporter.py
@@ -0,0 +1,116 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from flask import Blueprint
+from flask import request
+from flask import Response
+from itertools import imap
+import json
+import six
+from sqlalchemy import and_
+
+from fuel_analytics.api.app import app
+from fuel_analytics.api.app import db
+from fuel_analytics.api.db.model import ActionLog as AL
+from fuel_analytics.api.db.model import InstallationStructure as IS
+from fuel_analytics.api.db.model import OpenStackWorkloadStats as OSWL
+
+bp = Blueprint('dto', __name__)
+
+
+def row_as_dict(row):
+    result = {}
+    for column in row.__table__.columns:
+        result[column.name] = six.text_type(getattr(row, column.name))
+    return result
+
+
+def get_dict_param(name):
+    params = request.args.get(name)
+    if not isinstance(params, dict):
+        params = {}
+    return params
+
+
+def get_paging_params():
+    params = get_dict_param('paging_params')
+    return {
+        'limit': params.get('limit', app.config['JSON_DB_DEFAULT_LIMIT']),
+        'offset': params.get('offset', 0)
+    }
+
+
+@bp.route('/installation_info/<master_node_uid>', methods=['GET'])
+def get_installation_info(master_node_uid):
+    app.logger.debug("Fetching installation info for: %s", master_node_uid)
+    result = db.session.query(IS).filter(
+        IS.master_node_uid == master_node_uid).one()
+    dict_result = row_as_dict(result)
+    app.logger.debug("Installation info for: %s fetched", master_node_uid)
+    return Response(json.dumps(dict_result), mimetype='application/json')
+
+
+def _get_db_objs(model, sql_clauses, order_by, paging_params):
+    """Gets DB objects by sql_clauses
+    :param model: DB model
+    :param sql_clauses: collection of clauses for selecting DB objects
+    :param order_by: ordering for DB objects
+    :param paging_params: dictionary with limit, offset values
+    :return: generator on jsonyfied DB objects data
+    """
+    result = db.session.query(model).filter(and_(*sql_clauses)).\
+        order_by(order_by).limit(paging_params['limit']).\
+        offset(paging_params['offset']).all()
+    dicts_result = imap(json.dumps, imap(row_as_dict, result))
+    return dicts_result
+
+
+@bp.route('/oswls/<master_node_uid>', methods=['GET'])
+def get_oswls(master_node_uid):
+    paging_params = get_paging_params()
+    app.logger.debug("Fetching oswl info for: %s, paging prams: %s",
+                     master_node_uid, paging_params)
+    sql_clauses = (OSWL.master_node_uid == master_node_uid,)
+    oswls_data = _get_db_objs(OSWL, sql_clauses, OSWL.id.asc(),
+                              paging_params)
+    app.logger.debug("Oswl info for: %s, paging params: %s fetched",
+                     master_node_uid, paging_params)
+    return Response(oswls_data, mimetype='application/json')
+
+
+@bp.route('/oswls/<master_node_uid>/<resource_type>', methods=['GET'])
+def get_oswls_by_resource_type(master_node_uid, resource_type):
+    paging_params = get_paging_params()
+    app.logger.debug("Fetching oswl info for: %s, %s, paging params: %s",
+                     master_node_uid, resource_type, paging_params)
+    sql_clauses = (OSWL.master_node_uid == master_node_uid,
+                   OSWL.resource_type == resource_type)
+    oswls_data = _get_db_objs(OSWL, sql_clauses, OSWL.id.asc(),
+                              paging_params)
+    app.logger.debug("Oswl info for: %s, %s, paging prams: %s fetched",
+                     master_node_uid, resource_type, paging_params)
+    return Response(oswls_data, mimetype='application/json')
+
+
+@bp.route('/action_logs/<master_node_uid>', methods=['GET'])
+def get_action_logs(master_node_uid):
+    paging_params = get_paging_params()
+    app.logger.debug("Fetching action_logs for: %s, paging params: %s",
+                     master_node_uid, paging_params)
+    sql_clauses = (AL.master_node_uid == master_node_uid,)
+    action_logs_data = _get_db_objs(AL, sql_clauses, AL.id.asc(),
+                                    paging_params)
+    app.logger.debug("Action_logs for: %s, paging params: %s fetched",
+                     master_node_uid, paging_params)
+    return Response(action_logs_data, mimetype='application/json')
