7fccde7be7bba80b2495507dfba220a67b1d2bdc
Alexander Kislitsky <akislitsky@mirantis.com>
2016-01-20 18:23:47 +0300

    Release 8.0 added to filter in fuel-stats UI
    Change-Id: I53c4d60714ec69ff700abdc2a533bbe863f58e39
    Partial-Bug: #1533660

--- analytics/static/js/app.js
+++ analytics/static/js/app.js
@@ -18,7 +18,8 @@ function($, d3, D3pie, d3tip, nv, elasticsearch) {
         {name: '6.0 Technical Preview', filter: '6.0-techpreview'},
         {name: '6.0 GA', filter: '6.0'},
         {name: '6.1', filter: '6.1'},
-        {name: '7.0', filter: '7.0'}
+        {name: '7.0', filter: '7.0'},
+        {name: '8.0', filter: '8.0'}
     ];
     var currentRelease = releases[0].filter;
 
