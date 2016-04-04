7b8041abee9e84d1ffb9a1b392021854c2b4afdc
Alexander Kislitsky <akislitsky@mirantis.com>
2016-04-04 15:58:27 +0300

    Memory consumption reduced in fuel-stats analytics
    We had huge memory consumption on fetching OSWLs due to fetching
    clusters data in each OSWL row.
    
    Joined clusters data removed from each OSWL row. Explicit
    fetching of version info from clusters is introduced to CSV
    exporter.
    
    We still have issues with memory consumption in all
    reports generation. Thus we are temporary removing link
    on 'All reports' from web UI.
    
    Example uWSGI configuration changed to start http service.
    
    Change-Id: I66c97058c0f15f8a15b626ab1fa222106754a455
    Partial-Bug: #1564427

--- analytics/fuel_analytics/api/resources/csv_exporter.py
+++ analytics/fuel_analytics/api/resources/csv_exporter.py
@@ -65,16 +65,22 @@ def get_to_date():
                         default_value=datetime.utcnow().date())
 
 
-def get_inst_structures_query(from_date=None, to_date=None):
+def get_inst_structures_query(from_date=None, to_date=None, fields=()):
     """Composes query for fetching not filtered installation
     structures info with filtering by from and to dates and
     ordering by id. Installation structure is not filtered
     if is_filtered is False or None.
+
     :param from_date: filter from creation or modification date
     :param to_date: filter to creation or modification date
+    :param fields: fields to be filtered in query. All fields will
+    be fetched if fields is empty.
     :return: SQLAlchemy query
     """
-    query = db.session.query(IS)
+    if fields:
+        query = db.session.query(*fields)
+    else:
+        query = db.session.query(IS)
     query = query.filter(or_(
         IS.is_filtered == bool(False),  # workaround for PEP8 error E712
         IS.is_filtered.is_(None)))
@@ -94,8 +100,29 @@ def get_inst_structures():
     yield_per = app.config['CSV_DB_YIELD_PER']
     from_date = get_from_date()
     to_date = get_to_date()
-    return get_inst_structures_query(from_date=from_date,
-                                     to_date=to_date).yield_per(yield_per)
+    query = get_inst_structures_query(from_date=from_date,
+                                      to_date=to_date)
+    return query.yield_per(yield_per)
+
+
+def get_clusters_version_info():
+    """Returns dict of version info from clusters.
+
+    :return: dict of saved cluster versions with
+    structure {mn_uid: {cluster_id: version_info}}
+    """
+    yield_per = app.config['CSV_DB_YIELD_PER']
+    from_date = get_from_date()
+    to_date = get_to_date()
+    query = get_inst_structures_query(
+        from_date=from_date,
+        to_date=to_date,
+        fields=(IS.master_node_uid, IS.structure['clusters'].label('clusters'))
+    )
+    clusters_version_info = {}
+    for info in query.yield_per(yield_per):
+        _add_oswl_to_clusters_versions_cache(info, clusters_version_info)
+    return clusters_version_info
 
 
 def get_action_logs_query():
@@ -179,7 +206,6 @@ def get_oswls_query(resource_type, from_date=None, to_date=None):
         IS.creation_date.label('installation_created_date'),
         IS.modification_date.label('installation_updated_date'),
         IS.structure['fuel_release'].label('fuel_release_from_inst_info'),
-        IS.structure['clusters'].label('clusters'),
         IS.is_filtered).\
         join(IS, IS.master_node_uid == OSWS.master_node_uid).\
         filter(OSWS.resource_type == resource_type).\
@@ -188,17 +214,51 @@ def get_oswls_query(resource_type, from_date=None, to_date=None):
         query = query.filter(OSWS.created_date >= from_date)
     if to_date is not None:
         query = query.filter(OSWS.created_date <= to_date)
-    return query.order_by(OSWS.created_date)
+    # For proper handling of paging we must use additional ordering by id.
+    # In other case we will lose some OSWLs form the execution result.
+    query = query.order_by(OSWS.created_date, OSWS.id)
+    return query
 
 
 def get_oswls(resource_type):
     yield_per = app.config['CSV_DB_YIELD_PER']
-    app.logger.debug("Fetching %s oswls with yeld per %d",
+    app.logger.debug("Fetching %s oswls with yield per %d",
                      resource_type, yield_per)
     from_date = get_from_date()
     to_date = get_to_date()
-    return get_oswls_query(resource_type, from_date=from_date,
-                           to_date=to_date).yield_per(yield_per)
+    query = get_oswls_query(resource_type, from_date=from_date,
+                            to_date=to_date)
+    return query.yield_per(yield_per)
+
+
+def _add_oswl_to_clusters_versions_cache(inst_structure, clusters_versions):
+    """Adds oswl clusters version_info into clusters_versions cache.
+
+    :param inst_structure: InstallationStructure DB object
+    :type inst_structure: fuel_analytics.api.db.model.InstallationStructure
+    :param clusters_versions: cache for saving cluster versions with
+    structure {mn_uid: {cluster_id: version_info}}
+    :type clusters_versions: dict
+    """
+
+    mn_uid = inst_structure.master_node_uid
+
+    clusters = inst_structure.clusters or []
+    clusters_versions[mn_uid] = {}
+
+    for cluster in clusters:
+        fuel_version = cluster.get('fuel_version')
+        if not fuel_version:
+            continue
+
+        version_info = {'fuel_version': fuel_version}
+        release = cluster.get('release')
+        if release:
+            version_info['release_version'] = release.get('version')
+            version_info['release_os'] = release.get('os')
+            version_info['release_name'] = release.get('name')
+
+        clusters_versions[mn_uid][cluster['id']] = version_info
 
 
 @bp.route('/<resource_type>', methods=['GET'])
@@ -208,7 +268,10 @@ def oswl_to_csv(resource_type):
 
     exporter = OswlStatsToCsv()
     oswls = get_oswls(resource_type)
-    result = exporter.export(resource_type, oswls, get_to_date())
+
+    clusters_version_info = get_clusters_version_info()
+    result = exporter.export(resource_type, oswls, clusters_version_info,
+                             get_to_date())
 
     # NOTE: result - is generator, but streaming can not work with some
     # WSGI middlewares: http://flask.pocoo.org/docs/0.10/patterns/streaming/
@@ -229,10 +292,12 @@ def get_resources_types():
     return (row[0] for row in result)
 
 
-def get_all_reports(from_date, to_date):
+def get_all_reports(from_date, to_date, clusters_version_info):
     """Returns generator on all reports info.
     :param from_date: get reports from date
     :param to_date: get reports to date
+    :param clusters_version_info: dict with version_info fetched from
+    clusters
     :return: generator on sequence of tuples (report data, report name)
     """
     app.logger.debug("Getting all reports")
@@ -247,26 +312,27 @@ def get_all_reports(from_date, to_date):
         app.logger.debug("Getting report '%s'", resource_type)
         oswls = get_oswls_query(resource_type, from_date=from_date,
                                 to_date=to_date)
-        report = oswl_exporter.export(resource_type, oswls, to_date)
+        report = oswl_exporter.export(resource_type, oswls,
+                                      clusters_version_info, to_date)
         app.logger.debug("Report '%s' got", resource_type)
         yield report, '{}.csv'.format(resource_type)
 
     # Clusters report
     app.logger.debug("Getting clusters report")
-    inst_strucutres = get_inst_structures_query(from_date=from_date,
+    inst_structures = get_inst_structures_query(from_date=from_date,
                                                 to_date=to_date)
     query_action_logs = get_action_logs_query()
     action_logs = db.session.execute(query_action_logs,
                                      {'from_date': from_date,
                                       'to_date': to_date})
-    clusters = stats_exporter.export_clusters(inst_strucutres,
+    clusters = stats_exporter.export_clusters(inst_structures,
                                               action_logs)
     app.logger.debug("Clusters report got")
     yield clusters, CLUSTERS_REPORT_FILE
 
     # Plugins report
     app.logger.debug("Getting plugins report")
-    plugins = stats_exporter.export_plugins(inst_strucutres)
+    plugins = stats_exporter.export_plugins(inst_structures)
     app.logger.debug("Plugins report got")
     yield plugins, PLUGINS_REPORT_FILE
 
@@ -309,7 +375,9 @@ def all_reports():
     app.logger.debug("Handling all_reports get request")
     from_date = get_from_date()
     to_date = get_to_date()
-    reports = get_all_reports(from_date, to_date)
+
+    clusters_version_info = get_clusters_version_info()
+    reports = get_all_reports(from_date, to_date, clusters_version_info)
 
     name = 'reports_from{}_to{}'.format(get_from_date(), get_to_date())
     headers = {
